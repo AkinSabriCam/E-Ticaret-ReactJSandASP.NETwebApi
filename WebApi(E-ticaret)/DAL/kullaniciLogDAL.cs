b65bde717eaa4aabdf63ccd8d6aee4eac88c01de
İbrahim Doğruer <ibrahim-1997@hotmail.com>
2019-03-22 15:40:46 +0300

    Kullanıcı Loglama İşlemleri
    Kullanıcı loglama işlemleri gerçekleştirildi.

---
+++ WebApi(E-ticaret)/DAL/kullaniciLogDAL.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class kullaniciLogDAL
+    {
+        Models.Entities db = new Models.Entities();
+
+        public Models.logKullanici GetLogKullanici()
+        {
+            return db.logKullanici.FirstOrDefault(x => x.logKullaniciID == 1);
+        }
+
+        public bool PutUserLog()
+        {
+            var log = db.logKullanici.FirstOrDefault(x => x.logKullaniciID == 1);
+            if (log != null)
+            {
+                log.gunlukKayit++;
+                log.haftalikKayit++;
+                log.toplamKayit++;
+                db.SaveChanges();
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool PutLogDate()
+        {
+            var log = db.logKullanici.FirstOrDefault(x => x.logKullaniciID == 1);
+            if (log != null)
+            {
+                if ((log.gunlukTarih.Value.DayOfYear / 7) != ((DateTime.Now.DayOfYear) / 7))
+                {
+                    log.haftalikTarih++;
+                    log.haftalikKayit = 0;
+                    db.SaveChanges();
+                }
+                else if (log.gunlukTarih.Value.Day != DateTime.Now.Day)
+                {
+                    log.gunlukTarih = DateTime.Now;
+                    log.gunlukKayit = 0;
+                    db.SaveChanges();
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
